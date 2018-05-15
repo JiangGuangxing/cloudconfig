2e0bc8da049fe64c60479f48199ad2731c6959fa
姜广兴 <jiangguangxing@duanrong.com>
2018-05-15 19:05:09 +0800

    init

--- configclient/src/main/java/App.java
+++ configclient/src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableAutoConfiguration
 @RestController
+@RefreshScope
 public class App {
     @Value("${test}")
-    String name = "World";
+    private String name;
 
     @RequestMapping("/")
     public String home() {
-        return "Hello " + name;
+        return "hello " + name;
     }
 
     public static void main(String[] args) {
